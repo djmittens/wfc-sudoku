ea9da74d8ba3d26c9144806c168742fa2e4a49f0
Dj Mittens <niksgamin@gmail.com>
2022-07-01 01:38:43 -0700

    More refactoring, will split out nodes next

--- Assets/SudokuSolver.cs
+++ Assets/SudokuSolver.cs
@@ -9,14 +9,36 @@ public class SudokuSolver : MonoBehaviour
     public GameObject canvas;
     Node[] nodes;
     Stack<Node> propagations;
+    SudokuBoard c_board;
+    Superpositions c_superpositions;
 
     // Start is called before the first frame update
     void Start()
     {
         this.history = new Stack<BoardState>();
         this.propagations = new Stack<Node>();
-        var boardState = new BoardState();
-        boardState.board = new int[] {
+
+        this.nodes = new Node[81];
+        for (int i = 0; i < 81; i++)
+        {
+            var n = new Node();
+            n.id = i;
+            n.neighbors = new List<Node>();
+            n.propagations = this.propagations;
+            nodes[i] = n;
+        }
+        for (int i = 0; i < 81; i++)
+        {
+            for (int k = 0; k < 81; k++)
+            {
+                if (i != k && (CheckRow(k, i) + CheckCol(k, i) + CheckHood(k, i)) > 0)
+                {
+                    nodes[k].neighbors.Add(nodes[i]);
+                }
+            }
+        }
+
+        var boardState = new BoardState(new int[] {
             0,0,0,0,0,0,0,0,0,
             0,7,0,0,0,0,0,0,0,
             0,0,0,0,0,0,4,0,0,
@@ -26,50 +48,23 @@ public class SudokuSolver : MonoBehaviour
             0,0,0,0,0,0,0,0,0,
             0,0,2,0,0,0,0,0,0,
             0,0,0,0,0,0,3,0,0,
-        };
-        boardState.spos = new HashSet<int>[81];
+        }, this.nodes);
         this.history.Push(boardState);
 
-        this.nodes = new Node[81];
-        for (int i = 0; i < 81; i++)
+        this.c_board = gameObject.GetComponent<SudokuBoard>();
+        if (this.c_board != null)
         {
-            var n = new Node();
-            n.id = i;
-            n.hood = i / 3;
-            n.row = i / 9;
-            n.col = i % 9;
-            n.neighbors = new List<Node>();
-            n.propagations = this.propagations;
-            nodes[i] = n;
+            this.c_board.Init(canvas);
         }
-
-        var board = gameObject.GetComponent<SudokuBoard>();
-        if (board != null)
+        this.c_superpositions = gameObject.GetComponent<Superpositions>();
+        if (this.c_superpositions != null)
         {
-            board.Init(canvas);
-            board.UpdateState(boardState.board);
-        }
-        var spos = gameObject.GetComponent<Superpositions>();
-        if (spos != null)
-        {
-            for (int i = 0; i < 81; i ++) {
-                boardState.spos[i] = new HashSet<int>{1,2,3,4,5,6,7,8,9};
-            }
-
-            for (int i = 0; i < 81; i ++) {
-                if(boardState.board[i] > 0) {
-                    nodes[i].Set(boardState.board[i], boardState.spos);
-                }
-            }
-
-            spos.Init(canvas);
-            spos.RegisterOnclick((i, v) =>
+            this.c_superpositions.Init(canvas);
+            this.c_superpositions.RegisterOnclick((i, v) =>
             {
                 var st = history.Peek().spos;
                 nodes[i].Set(v, st);
-                nodes[i].Propagate(boardState);
-                spos.UpdateState(boardState.spos);
-                board.UpdateState(boardState.board);
+                UpdateBoardState();
             });
         }
 
@@ -77,15 +72,15 @@ public class SudokuSolver : MonoBehaviour
         // For explanation https://www.desmos.com/calculator/rrucuhps2t
         int CheckRow(int candidate, int origin)
         {
-            return Clamp(candidate/ 9 - origin / 9);
+            return Clamp(candidate / 9 - origin / 9);
         }
         int CheckCol(int candidate, int origin)
         {
-            return Clamp(candidate % 9 - origin % 9);
+            return Clamp((candidate - origin) % 9);
         }
         int CheckHood(int candidate, int origin)
         {
-            return CheckK(candidate, origin) * Clamp((candidate / (9 * 3)) - (origin/ (9 * 3)));
+            return CheckK(candidate, origin) * Clamp((candidate / (9 * 3)) - (origin / (9 * 3)));
         }
         int CheckK(int candidate, int origin)
         {
@@ -107,20 +102,19 @@ public class SudokuSolver : MonoBehaviour
             }
         }
 
-        for (int i = 0; i < 81; i++)
-        {
-            for (int k = 0; k < 81; k++)
-            {
-                if (i != k && (CheckRow(k, i) + CheckCol(k, i) + CheckHood(k, i) ) > 0)
-                {
-                    nodes[k].neighbors.Add(nodes[i]);
-                }
-            }
-        }
 
-        nodes[0].Propagate(boardState);
-        board.UpdateState(boardState.board);
-        spos.UpdateState(boardState.spos);
+        UpdateBoardState();
+    }
+
+    void UpdateBoardState() {
+        var st = history.Peek();
+        st.Propagate(propagations);
+        if(this.c_board != null) {
+            this.c_board.UpdateState(st.board);
+        }
+        if(this.c_superpositions != null) {
+            this.c_superpositions.UpdateState(st.spos);
+        }
     }
 
     // Update is called once per frame
@@ -129,76 +123,84 @@ public class SudokuSolver : MonoBehaviour
 
     }
 
-    public class BoardState
+    class BoardState
     {
         public int[] board;
         public HashSet<int>[] spos; // super positions at that node
-    }
-}
-
-class Node
-{
-    public int id = 0;
-    public int hood = 0;
-    public int row = 0;
-    public int col = 0;
-    // public HashSet<int> spos = new HashSet<int>();
-
-    public IList<Node> neighbors;
-
-    public Stack<Node> propagations;
-
-    int Entropy(HashSet<int>[] spos)
-    {
-        return spos[id].Count;
-    }
-
-    int val(HashSet<int>[] spos)
-    {
-        return spos[id].Single();
-    }
-
-    bool Visit(int pos, HashSet<int>[] spos)
-    {
-        if (spos[id].Contains(pos))
+        public BoardState(int[] board, Node[] nodes)
         {
-            spos[id].Remove(pos);
-            var e = Entropy(spos);
-            if (e == 1)
+            this.board = new int[81];
+            this.spos = new HashSet<int>[81];
+            for (int i = 0; i < 81; i++)
             {
-                Set(val(spos), spos);
-            }
-            else if (e == 0)
-            {
-                Debug.LogError("Something fucked up");
-                return false;
+                spos[i] = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                if(board[i] > 0) {
+                    nodes[i].Set(board[i], spos);
+                }
             }
         }
-        return true;
-    }
-
-    public void Set(int v, HashSet<int>[] spos)
-    {
-        spos[id].Clear();
-        spos[id].Add(v);
-        propagations.Push(this);
-    }
-
-    public bool Propagate(SudokuSolver.BoardState st)
-    {
-        var res = true;
-
-        while (propagations.Count > 0)
+        public bool Propagate(Stack<Node> propagations)
         {
-            var node = propagations.Pop();
-            var pos = node.val(st.spos);
-            st.board[node.id] = pos;
-            foreach (var n in node.neighbors)
+            var res = true;
+
+            while (propagations.Count > 0)
             {
-                res &= n.Visit(pos, st.spos);
+                var node = propagations.Pop();
+                var pos = node.val(this.spos);
+                this.board[node.id] = pos;
+                foreach (var n in node.neighbors)
+                {
+                    res &= n.Visit(pos, this.spos);
+                }
             }
+            return res;
         }
-        return res;
     }
 
+    class Node
+    {
+        public int id = 0;
+
+        public IList<Node> neighbors;
+
+        public Stack<Node> propagations;
+
+        int Entropy(HashSet<int>[] spos)
+        {
+            return spos[id].Count;
+        }
+
+        public int val(HashSet<int>[] spos)
+        {
+            return spos[id].Single();
+        }
+
+        public bool Visit(int pos, HashSet<int>[] spos)
+        {
+            if (spos[id].Contains(pos))
+            {
+                spos[id].Remove(pos);
+                var e = Entropy(spos);
+                if (e == 1)
+                {
+                    Set(val(spos), spos);
+                }
+                else if (e == 0)
+                {
+                    Debug.LogError("Something fucked up");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void Set(int v, HashSet<int>[] spos)
+        {
+            spos[id].Clear();
+            spos[id].Add(v);
+            propagations.Push(this);
+        }
+
+
+    }
 }
